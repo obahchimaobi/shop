5a2294765ca97b8e5b2d2b85aacda582bc83c602
Chimaobi Anthony <102151079+ChunkBraniac@users.noreply.github.com>
2024-05-26 22:20:29 +0100

    new

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,109 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<!-- index28:48-->
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Home Version One || limupa - Digital Products Store eCommerce Bootstrap 4 Template</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="{{ url('images/favicon.png') }}">
+    <!-- Material Design Iconic Font-V2.2.0 -->
+    <link rel="stylesheet" href="{{ url('css/material-design-iconic-font.min.css') }}">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ url('css/font-awesome.min.css') }}">
+    <!-- Font Awesome Stars-->
+    <link rel="stylesheet" href="{{ url('css/fontawesome-stars.css') }}">
+    <!-- Meanmenu CSS -->
+    <link rel="stylesheet" href="{{ url('css/meanmenu.css') }}">
+    <!-- owl carousel CSS -->
+    <link rel="stylesheet" href="{{ url('css/owl.carousel.min.css') }}">
+    <!-- Slick Carousel CSS -->
+    <link rel="stylesheet" href="{{ url('css/slick.css') }}">
+    <!-- Animate CSS -->
+    <link rel="stylesheet" href="{{ url('css/animate.css') }}">
+    <!-- Jquery-ui CSS -->
+    <link rel="stylesheet" href="{{ url('css/jquery-ui.min.css') }}">
+    <!-- Venobox CSS -->
+    <link rel="stylesheet" href="{{ url('css/venobox.css') }}">
+    <!-- Nice Select CSS -->
+    <link rel="stylesheet" href="{{ url('css/nice-select.css') }}">
+    <!-- Magnific Popup CSS -->
+    <link rel="stylesheet" href="{{ url('css/magnific-popup.css') }}">
+    <!-- Bootstrap V4.1.3 Fremwork CSS -->
+    <link rel="stylesheet" href="{{ url('css/bootstrap.min.css') }}">
+    <!-- Helper CSS -->
+    <link rel="stylesheet" href="{{ url('css/helper.css') }}">
+    <!-- Main Style CSS -->
+    <link rel="stylesheet" href="{{ url('style.css') }}">
+    <!-- Responsive CSS -->
+    <link rel="stylesheet" href="{{ url('css/responsive.css') }}">
+    <!-- Modernizr js -->
+    <script src="{{ url('js/vendor/modernizr-2.8.3.min.js') }}"></script>
+</head>
+
+<body>
+    <!--[if lt IE 8]>
+  <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="{{ url('http://browsehappy.com/') }}">upgrade your browser</a> to improve your experience.</p>
+ <![endif]-->
+    <!-- Begin Body Wrapper -->
+    <div class="body-wrapper">
+
+        @include('layouts._header')
+
+        @yield('content')
+
+        @include('layouts._footer')
+
+    </div>
+    <!-- Body Wrapper End Here -->
+    <!-- jQuery-V1.12.4 -->
+    <script src="{{ url('js/vendor/jquery-1.12.4.min.js') }}"></script>
+    <!-- Popper js -->
+    <script src="{{ url('js/vendor/popper.min.js') }}"></script>
+    <!-- Bootstrap V4.1.3 Fremwork js -->
+    <script src="{{ url('js/bootstrap.min.js') }}"></script>
+    <!-- Ajax Mail js -->
+    <script src="{{ url('js/ajax-mail.js') }}"></script>
+    <!-- Meanmenu js -->
+    <script src="{{ url('js/jquery.meanmenu.min.js') }}"></script>
+    <!-- Wow.min js -->
+    <script src="{{ url('js/wow.min.js') }}"></script>
+    <!-- Slick Carousel js -->
+    <script src="{{ url('js/slick.min.js') }}"></script>
+    <!-- Owl Carousel-2 js -->
+    <script src="{{ url('js/owl.carousel.min.js') }}"></script>
+    <!-- Magnific popup js -->
+    <script src="{{ url('js/jquery.magnific-popup.min.js') }}"></script>
+    <!-- Isotope js -->
+    <script src="{{ url('js/isotope.pkgd.min.js') }}"></script>
+    <!-- Imagesloaded js -->
+    <script src="{{ url('js/imagesloaded.pkgd.min.js') }}"></script>
+    <!-- Mixitup js -->
+    <script src="{{ url('js/jquery.mixitup.min.js') }}"></script>
+    <!-- Countdown -->
+    <script src="{{ url('js/jquery.countdown.min.js') }}"></script>
+    <!-- Counterup -->
+    <script src="{{ url('js/jquery.counterup.min.js') }}"></script>
+    <!-- Waypoints -->
+    <script src="{{ url('js/waypoints.min.js') }}"></script>
+    <!-- Barrating -->
+    <script src="{{ url('js/jquery.barrating.min.js') }}"></script>
+    <!-- Jquery-ui -->
+    <script src="{{ url('js/jquery-ui.min.js') }}"></script>
+    <!-- Venobox -->
+    <script src="{{ url('js/venobox.min.js') }}"></script>
+    <!-- Nice Select js -->
+    <script src="{{ url('js/jquery.nice-select.min.js') }}"></script>
+    <!-- ScrollUp js -->
+    <script src="{{ url('js/scrollUp.min.js') }}"></script>
+    <!-- Main/Activator js -->
+    <script src="{{ url('js/main.js') }}"></script>
+</body>
+
+<!-- index30:23-->
+
+</html>
